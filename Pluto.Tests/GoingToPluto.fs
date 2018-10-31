fc14582d69564b6d1820fe2624f296dd0a9ddc2b
Mark Gray <mgray@globalreachgroup.com>
2018-10-31 07:11:43 +0000

    Adding first failing test to set out my initial idea, let's see how it develops

---
+++ Pluto.Tests/GoingToPluto.fs
@@ -0,0 +1,17 @@
+module Tests
+
+open Expecto
+open Exercise
+
+[<Tests>]
+let tests =
+  testList "Going to Pluto" [
+    testCase "Starting loc is 0,0 N" <| fun _ ->
+      let pluto = { XLength = 100; YLength = 100 }
+
+      let rover = landOn(pluto)
+
+      let loc = rover.getLoc()
+
+      Expect.equal loc { X = 0; Y = 0; Heading = N } "I compute, therefore I am."
+  ]
